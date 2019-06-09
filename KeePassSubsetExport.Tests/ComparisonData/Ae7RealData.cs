e0f6a929c923c9103f54d0a7854d4f7d229d32b2
lukeIam <2lukeiam@gmail.com>
2019-06-09 18:04:50 +0200

    Add: Expired job entries are not executed (closes #27)

---
+++ KeePassSubsetExport.Tests/ComparisonData/Ae7RealData.cs
@@ -0,0 +1,8 @@
+﻿
+namespace KeePassSubsetExport.Tests.ComparisonData
+{
+    internal static class Ae7RealData
+    {
+        public static string Db => "A_E7.kdbx";
+    }
+}
--- KeePassSubsetExport.Tests/DataContainer/TestSettings.cs
+++ KeePassSubsetExport.Tests/DataContainer/TestSettings.cs
@@ -6,14 +6,18 @@ namespace KeePassSubsetExport.Tests.DataContainer
         internal string RootPath { get; set; }
         internal string DbAFilesPath { get; set; }
         internal string DbBFilesPath { get; set; }
+        internal string DbCFilesPath { get; set; }
 
         internal string DbAPath { get; set; }
         internal string DbBPath { get; set; }
+        internal string DbCPath { get; set; }
         internal string DbMainPw { get; set; }
 
 
         internal string KeyTestAPath { get; set; }
         internal string KeyTestBPath { get; set; }
+        internal string KeyTestCPath { get; set; }
+
         internal string DbTestPw { get; set; }
     }
 }
