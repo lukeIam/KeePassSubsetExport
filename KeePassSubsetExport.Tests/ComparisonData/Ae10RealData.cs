f9ea09e613ece81d0d3f4945deeaaa04d4cf6285
lukeIam <2lukeiam@gmail.com>
2020-03-23 16:51:23 +0100

    Fix: Ae10RealData.cs is missing

---
+++ KeePassSubsetExport.Tests/ComparisonData/Ae10RealData.cs
@@ -0,0 +1,12 @@
+﻿using KeePassSubsetExport.Tests.DataContainer;
+
+namespace KeePassSubsetExport.Tests.ComparisonData
+{
+    internal static class Ae10RealData
+    {
+        public static string Db => "A_E10.kdbx";
+
+        public static TestKdfValues Kdf => Ae1RealData.Kdf;
+        public static TestGroupValues Data => Ae1RealData.Data;
+    }
+}
